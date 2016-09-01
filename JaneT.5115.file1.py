625c22b901c8ae04c44935c96971be4b16e4633d
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-01 21:45:37 +0300

    Commit

--- JaneT.5115.file1.py
+++ JaneT.5115.file1.py
@@ -1,3 +1,30 @@
 
 __author__ = 'student'
 print("Hello? World")
+a=int(input())
+b=int(input())
+c=(a**2+b**2)**0.5
+print(c)
+d=int(input())
+e=d+1
+print("The next number for the number", d, "is", e)
+e=d-1
+print("The previous number for the number", d, "is", e)
+f=int(input())
+f=f%10
+print(f)
+n=int(input())
+n=n%100
+n=n//10
+print(n)
+s=input()
+print(s[2])
+print(s[-2])
+print(s[0:5])
+print(s[0:-2])
+print(s[0:len(s):2])
+print(s[1:len(s):2])
+print(s[0::-1])
+h=s[0::-1]
+print(h[0:len(h):2])
+print(len(s))
